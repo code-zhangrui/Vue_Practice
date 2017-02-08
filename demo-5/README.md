e34ad12ee1402d8c371598496a4707eef601d20e
code-zhangrui <code.zhangrui@gmail.com>
2017-02-08 19:24:03 +0800

    key change

---
+++ demo-5/README.md
@@ -0,0 +1,3 @@
+# vue-demos
+# npm i babel-loader babel-core babel-preset-es2015 babel-preset-react
+# npm install leancloud-storage --save
--- demo-5/app.js
+++ demo-5/app.js
@@ -24,12 +24,18 @@ var app = new Vue({
         // onbeforeunload文档：https://developer.mozilla.org/zh-CN/docs/Web/API/Window/onbeforeunload
         window.onbeforeunload = () => {
             let dataString = JSON.stringify(this.todoList) // JSON 文档: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON
-            window.localStorage.setItem('myTodos', dataString) // 看文档https://developer.mozilla.org/zh-CN/docs/Web/API/Window/localStorage
-        }
 
-        let oldDataString = window.localStorage.getItem('myTodos')
-        let oldData = JSON.parse(oldDataString)
-        this.todoList = oldData || []
+            var AVTodos = AV.Object.extend('AllTodos');
+            var avTodos = new AVTodos();
+            avTodos.set('content', dataString);
+            avTodos.save().then(function(todo) {
+                 // 成功保存之后，执行其他逻辑.
+                console.log('保存成功');
+            }, function(error) {
+                 // 异常处理
+                console.error('保存失败');
+            });
+        }
 
         this.currentUser = this.getCurrentUser();
 
