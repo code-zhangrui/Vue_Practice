78160baa72441947ed4813e13b67aa237f3f1319
code-zhangrui <code.zhangrui@gmail.com>
2017-02-18 13:54:48 +0800

    task7

---
+++ resumer/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class=page>
+     <header>
+       <Topbar/>
+     </header>
+     <main>
+       <ResumeEditor/>
+       <ResumePreview/>
+     </main>
+   </div>
+ </template>
+
+ <script>
+ import 'normalize.css/normalize.css'
+ import './assets/reset.css'
+ import Topbar from './components/Topbar'
+ import ResumeEditor from './components/ResumeEditor'
+ import ResumePreview from './components/ResumePreview'
+
+ export default {
+   name: 'app',
+   data: function(){
+     return {
+       text: '你好'
+     }
+
+  },
+  components: {Topbar, ResumeEditor, ResumePreview}
+ }
+ </script>
+ <style>
+    .page{
+     height: 100vh;
+     display: flex;
+     flex-direction: column;
+     background: #EAEBEC;
+    }
+    .page>main{
+      flex-grow: 1;
+    }
+
+    .page>main{
+      min-width: 1024px;
+      max-width: 1440px;
+      margin-top: 16px;
+      margin-bottom: 16px;
+      margin-bottom: 16px;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 16px;
+
+      width: 100%;
+      align-self: center;
+    }
+
+    #resumeEditor{
+      width: 35%;
+      background: #444;
+    }
+    #resumePreview{
+
+      flex-grow: 1;
+      margin-left: 16px;
+      background: #777;
+    }
+   </style>
---
+++ resumer/src/components/ResumeEditor.vue
@@ -0,0 +1,18 @@
+<template>
+   <div id="resumeEditor">
+     I am resumeEditor
+   </div>
+ </template>
+
+ <script>
+ export default {
+   name: 'ResumeEditor'
+ }
+ </script>
+
+ <style>
+   #resumeEditor{
+    background:#ffffff;
+    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+   }
+ </style>
