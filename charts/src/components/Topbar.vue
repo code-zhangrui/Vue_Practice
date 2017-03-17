164f02a0c50ed4a0d3a09a58b16e137dc5a38b1a
code-zhangrui <code.zhangrui@gmail.com>
2017-03-17 15:10:55 +0800

    charts

---
+++ charts/src/components/Topbar.vue
@@ -0,0 +1,36 @@
+<template>
+ <div id="topbar">
+   <div class="wrapper">
+     <span class="logo">Resume</span>
+   </div>
+  </div>
+ </template>
+
+<script>
+ export default{
+ 	name:'Topbar'
+ }
+</script>
+
+<style>
+ #topbar{
+ 	background:#fff;
+ 	box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+ }
+ #topbar>.wrapper{
+    min-width:1024px;
+    max-width:1440px;
+    margin:0 auto;
+    height:64px;
+ }
+ #topbar>.wrapper{
+ display:flex;
+ justify-content:space-between;
+ align-items:center;
+ padding:0 16px;
+ }
+ #topbar .logo{
+ font-size:24px;
+ color:#000;
+ }
+ </style>
