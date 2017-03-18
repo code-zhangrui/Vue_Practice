c740b0ce6dc5149e998467017f0d36443c47c392
code-zhangrui <code.zhangrui@gmail.com>
2017-03-18 11:10:01 +0800

    过了一遍charts，除了Leancloud相关功能

--- charts/src/App.vue
+++ charts/src/App.vue
@@ -49,7 +49,7 @@ export default {
  }
 }
 #resumeEditor{
-  width:35%;
+  min-width:35%;
   background:#444;
 }
 #resumePreview{
--- charts/src/store/index.js
+++ charts/src/store/index.js
@@ -1,5 +1,6 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
+import objectPath from "object-path"
 
 Vue.use(Vuex)
 
@@ -10,20 +11,20 @@ export default new Vuex.Store({
         config:
            [
              { field: 'profile'},
-             { field: 'work history'},
+             { field: 'workHistory'},
              { field: 'education'},
              { field: 'projects'},
              { field: 'awards'},
              { field: 'contacts'},
            ],
-        profile:{name:'',city:'',title:''},
-        'work history': [
+        profile:{name:'张睿',city:'杭州',title:'前端工程师',birthday:'1984-08-21'},
+        workHistory: [
              { company: 'AL', content: '我的第二份工作是' },
              { company: 'TX', content: '我的第一份工作是' },
            ],
            education: [
-             { school: 'AL', content: '文字' },
-             { school: 'TX', content: '文字' },
+             { school: '苏州', content: '文字' },
+             { school: '汉阳', content: '文字' },
            ],
            projects: [
              { name: 'project A', content: '文字' },
@@ -41,7 +42,10 @@ export default new Vuex.Store({
 	},
 	mutations:{
 		switchTab(state,payload){
-			state.selected=payload
+			state.selected = payload
+		},
+		updateResume(state,{path,value}){
+			objectPath.set(state.resume,path,value)
 		}
 	}
 })
